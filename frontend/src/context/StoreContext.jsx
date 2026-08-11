67dc65203bacfb316afd5a7cf860ca243fdad3c7
Michael <oolayinka29@gmail.com>
2026-08-11 19:46:45 +0100

    login logout icon

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,11 +1,13 @@
 import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
-export const StoreContext = createContext(null);
+export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
 
   const [cartItems, setCartItems] = useState({});
+  const url = "http://localhost:4000"
+  const [token, setToken] = useState("");
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
@@ -38,7 +40,11 @@ const StoreContextProvider = (props) => {
     setCartItems,
     addToCart,
     removeFromCart,
-    getTotalCartAmount
+    getTotalCartAmount,
+    url,
+    token,
+    setToken
+
   }
 
   return (
